ce96d084f0c6d722f5161a559bdd75181cc8accb
Laxerus <emrecancavus2@gmail.com>
2026-09-14 04:27:28 +0300

    Test Save Scout directory heuristics

--- app/src/test/java/dev/laxerus/omnifiles/scout/SaveScoutServiceTest.kt
+++ app/src/test/java/dev/laxerus/omnifiles/scout/SaveScoutServiceTest.kt
@@ -15,4 +15,18 @@ class SaveScoutServiceTest {
         assertFalse(SaveScoutService.isValidPackageName("com.game;rm"))
         assertFalse(SaveScoutService.isValidPackageName("com game.app"))
     }
+
+    @Test fun recognizesCommonSaveDirectoryNames() {
+        assertTrue(SaveScoutService.isLikelySaveDirectoryName("SaveGames"))
+        assertTrue(SaveScoutService.isLikelySaveDirectoryName("save_data"))
+        assertTrue(SaveScoutService.isLikelySaveDirectoryName("UserData01"))
+        assertTrue(SaveScoutService.isLikelySaveDirectoryName("UE4Game"))
+        assertTrue(SaveScoutService.isLikelySaveDirectoryName("Worlds"))
+    }
+
+    @Test fun ignoresGenericContentDirectories() {
+        assertFalse(SaveScoutService.isLikelySaveDirectoryName("cache"))
+        assertFalse(SaveScoutService.isLikelySaveDirectoryName("textures"))
+        assertFalse(SaveScoutService.isLikelySaveDirectoryName("downloads"))
+    }
 }
