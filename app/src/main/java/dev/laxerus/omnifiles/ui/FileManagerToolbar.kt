b839193a1b298ce84c777d8c9e342408d33f6fda
Laxerus <emrecancavus2@gmail.com>
2026-09-15 01:25:23 +0300

    Add file manager tools toolbar

---
+++ app/src/main/java/dev/laxerus/omnifiles/ui/FileManagerToolbar.kt
@@ -0,0 +1,23 @@
+package dev.laxerus.omnifiles.ui
+
+import android.content.Context
+import android.content.Intent
+import android.util.AttributeSet
+import com.google.android.material.appbar.MaterialToolbar
+import dev.laxerus.omnifiles.R
+
+class FileManagerToolbar @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = com.google.android.material.R.attr.toolbarStyle
+) : MaterialToolbar(context, attrs, defStyleAttr) {
+
+    init {
+        inflateMenu(R.menu.menu_file_browser)
+        setOnMenuItemClickListener { item ->
+            if (item.itemId != R.id.actionSettingsTools) return@setOnMenuItemClickListener false
+            context.startActivity(Intent(context, MainActivity::class.java))
+            true
+        }
+    }
+}
