e24f5e499c891e1a07f75765cf28bf9eff371218
Laxerus <emrecancavus2@gmail.com>
2026-09-15 09:14:28 +0300

    Run bounded startup cache maintenance

--- app/src/main/java/dev/laxerus/omnifiles/OmniFilesApp.kt
+++ app/src/main/java/dev/laxerus/omnifiles/OmniFilesApp.kt
@@ -2,10 +2,18 @@ package dev.laxerus.omnifiles
 
 import android.app.Application
 import com.google.android.material.color.DynamicColors
+import dev.laxerus.omnifiles.maintenance.StartupMaintenance
 
 class OmniFilesApp : Application() {
     override fun onCreate() {
         super.onCreate()
         DynamicColors.applyToActivitiesIfAvailable(this)
+        Thread(
+            { runCatching { StartupMaintenance.prune(cacheDir) } },
+            "omnifiles-startup-maintenance",
+        ).apply {
+            isDaemon = true
+            start()
+        }
     }
 }
