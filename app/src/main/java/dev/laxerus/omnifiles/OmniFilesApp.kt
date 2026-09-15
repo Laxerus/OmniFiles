d756118155c215702220413fa87141e732c2f8d7
Laxerus <emrecancavus2@gmail.com>
2026-09-15 09:43:54 +0300

    Run opt-in safe cleanup from startup maintenance

--- app/src/main/java/dev/laxerus/omnifiles/OmniFilesApp.kt
+++ app/src/main/java/dev/laxerus/omnifiles/OmniFilesApp.kt
@@ -2,6 +2,7 @@ package dev.laxerus.omnifiles
 
 import android.app.Application
 import com.google.android.material.color.DynamicColors
+import dev.laxerus.omnifiles.maintenance.AutoCleanupManager
 import dev.laxerus.omnifiles.maintenance.StartupMaintenance
 
 class OmniFilesApp : Application() {
@@ -9,7 +10,10 @@ class OmniFilesApp : Application() {
         super.onCreate()
         DynamicColors.applyToActivitiesIfAvailable(this)
         Thread(
-            { runCatching { StartupMaintenance.prune(cacheDir) } },
+            {
+                runCatching { StartupMaintenance.prune(cacheDir) }
+                runCatching { AutoCleanupManager.runIfDue(this@OmniFilesApp) }
+            },
             "omnifiles-startup-maintenance",
         ).apply {
             isDaemon = true
